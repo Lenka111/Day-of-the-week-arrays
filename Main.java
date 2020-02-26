7ab4a763732a183f3322594193b9a587a26e6912
Elena Voinu <37947819+Lenka111@users.noreply.github.com>
2020-02-26 11:28:08 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,36 @@
+//Elena Voinu
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String [] array = {"Monday","Tuesday","W","TH","F","S","Sunday"};
+
+        System.out.println("Enter a number from 1 to 7 to get the day of the week: ");
+        int number = sc.nextInt();
+
+        //validation
+        if(number <=0 || number >=8){
+            System.out.println("Invalid value");
+        }
+        else{
+
+        System.out.println("You entered: " + number + ", " + getDayOfTheWeek(array, number));}
+
+    }
+
+    private static String getDayOfTheWeek(String[] array, int number) {
+
+        //initialize string that will be the day in the array
+        String dayOfTheWeek = null;
+
+        for(int i = 0; i < array.length; i++){
+            //if index number equals user's input-1, store the value stored at that index into the string variable dayOfTheWeek
+            if(i == number-1){
+                 dayOfTheWeek = array[i];
+            }
+        }
+        return  dayOfTheWeek;
+    }
+}
